8aeffdea5808d9f225d0ca8439f31bdccf373af9
Kamil <bizkamiladamski@gmail.com>
2018-06-12 19:31:00 +0000

    add dependencies in to model

--- models/event.js
+++ models/event.js
@@ -9,6 +9,21 @@ const EventSchema= new mongoose.Schema({
       endDay:{type:Date},
       image: String,
       cathegory: String,
+      price:String,
+      createdAt:{type:Date,default:Date.now},
+      author: {
+        id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User"            
+        },
+        username: String
+      },
+      comments:[
+         {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"Comment"
+         }
+      ]
 });
 
 module.exports=mongoose.model("Event",EventSchema);
--- models/user.js
+++ models/user.js
@@ -7,6 +7,7 @@ const   UserSchema= new mongoose.Schema({
         firstName: String,
         lastName: String,
         email: String,
+        createdAt:{type:Date,default:Date.now},
         isAdmin:{type: Boolean, default:false}
 });
 
